2f1141fc814ce232d832d8acd800bf8470f05a7f
unknown <hicksey14@gmail.com>
2026-06-16 01:46:30 -0600

    implement the dynamic border color on avatar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const border_slide = document.querySelector("#brdr-range");
+const border_clrpck = document.querySelector("#clr-pick");
 const border_rad = document.querySelector("#avat-img");
 border_slide.addEventListener("change", (event) => {
     console.log(event.target.value);
     console.log(border_rad);
-    border_rad.style = `border-radius:${Number(event.target.value)}%`;
+    // border_rad.style = `border-radius:${Number(event.target.value)}%`;
+    border_rad.style.borderRadius = Number(event.target.value) + "%";
+});
+
+border_clrpck.addEventListener("change", event => {
+  console.log(event.target.value);
+  border_rad.style.borderColor = event.target.value;
 })

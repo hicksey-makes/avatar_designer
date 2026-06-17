996738356a97680106c11644613680b4de016e47
unknown <hicksey14@gmail.com>
2026-06-16 23:55:13 -0600

    implement avator image buttons change

--- src/index.js
+++ src/index.js
@@ -11,4 +11,23 @@ border_slide.addEventListener("change", (event) => {
 border_clrpck.addEventListener("change", event => {
   console.log(event.target.value);
   border_rad.style.borderColor = event.target.value;
-})
+});
+
+const btnImg1 = document.querySelector("#btn-img1");
+const btnImg2 = document.querySelector("#btn-img2");
+const btnImg3 = document.querySelector("#btn-img3");
+
+const avImg = document.querySelector("#avat-img");
+
+btnImg1.addEventListener("click", (event) => {
+  console.log(event.target.dataset.image);
+  avImg.src = event.target.dataset.image;
+});
+btnImg2.addEventListener("click", (event) => {
+  console.log(event.target.dataset.image);
+  avImg.src = event.target.dataset.image;
+});
+btnImg3.addEventListener("click", (event) => {
+  console.log(event);
+  avImg.src = event.target.dataset.image;
+});
